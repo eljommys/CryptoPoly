c7c17eea1ffeac6f784b9c54987d253709430203
jserrano <jserrano@student.42madrid.com>
2022-02-20 20:15:00 +0100

    current progress 70%

--- contracts/CryptoPoly.sol
+++ contracts/CryptoPoly.sol
@@ -8,6 +8,7 @@ contract CryptoPoy {
 
 	//enum Colors{BROWN, CIAN, MAGENTA, ORANGE, RED, YELLOW, GREEN, BLUE,
 	//			SERVICE, STATION, COMMUNITY, LUCKY}
+	uint256[12] colorsAmounts = [2, 3, 3, 3, 3, 3, 3, 2, 2, 4, 3, 3];
 
 	//These two must be a URI
 	string public communityCardsURI;
@@ -22,9 +23,9 @@ contract CryptoPoy {
 		uint256 slot;
 		uint256	isInJail;
 		address	wallet;
-		uint256[12] heritage; //the order is the "Colors" one
 		uint256[] slotsOwned;
-		//mapping(Colors => uint256) heritage;
+		uint256 houses;
+		uint256 hotels;
 	}
 
 	//REFERENCE (http://www.jdawiseman.com/papers/trivia/monopoly-rents.html)
@@ -34,7 +35,6 @@ contract CryptoPoy {
 		uint256 progress; //site-only, 1h, 2h, 3h, 4houses & hotel
 		uint256 cost;
 		uint256[6] rent;
-		//Colors color;
 		uint256 color; //the order is the "Colors" one
 	}
 
@@ -51,11 +51,19 @@ contract CryptoPoy {
 	uint256 public luckyCardsAmount = 20; //TODO: get actual amount of lucky and community cards
 	uint256 public communityCardsAmount = 10;
 
+	uint256 public houses = 32;
+	uint256 public hotels = 12;
+
 	uint256 public turnIndex;
 	bool	public hasRolled;
 	uint256 public auctionIndex; //above zero if an auction is in progress
 	uint256 public auctionPrice;
 	uint256 public auctionOwner; //starting from 1
+
+	uint256 public tradeFrom;
+	uint256 public tradeTo;
+	uint256 public tradeAmount;
+	uint256 public tradeItem;
 
 //===========================================================================================
 	//TODO: manage time, end of game, reset of variables, progress of houses, cards, when a player is in bankrupcy
@@ -94,8 +102,7 @@ contract CryptoPoy {
 	}
 
 	constructor(string memory _communityCardsURI, string memory _luckyCardsURI,
-				uint256[40] memory _costs, uint256[40][6] memory _rents, uint256[40] memory _colors,
-				uint256 _enteringPrice) {
+				uint256[40] memory _costs, uint256[40][6] memory _rents, uint256[40] memory _colors) {
 		communityCardsURI = _communityCardsURI;
 		luckyCardsURI = _luckyCardsURI;
 		for (uint256 i = 0; i < 40; i++) {
@@ -104,14 +111,14 @@ contract CryptoPoy {
 				slots[i].rent[j] = _rents[i][j];
 			slots[i].color = _colors[i];
 		}
-		enteringPrice = _enteringPrice;
 	}
 
 	function join_party() public payable notStarted {
-		Player memory currentPlayer;
-
 		require(playersAmount < maxPlayersAmount, "Game is full!");
 		require(msg.value == enteringPrice, "Please enter the exact entering price!");
+
+		Player memory currentPlayer;
+
 		currentPlayer.balance = 1500;
 		currentPlayer.wallet = msg.sender;
 		playersAmount++;
@@ -138,21 +145,43 @@ contract CryptoPoy {
 	}
 
 	function buy(bool _yes) public started onlyTurn noAuction afterRoll {
-		uint256 playerIndex = getPlayer[msg.sender] - 1;
 		Slot storage currentSlot = slots[auctionIndex];
 
 		require(currentSlot.owner == 0, "This property has already an owner!");
+		uint256 playerIndex = getPlayer[msg.sender] - 1;
 
 		if (_yes == true && timeLimit + turnTime < block.timestamp) {
 			require(players[playerIndex].balance > currentSlot.cost, "You have insufficient funds!");
 			players[playerIndex].balance -= currentSlot.cost;
-			players[playerIndex].heritage[currentSlot.color]++;
+			//players[playerIndex].heritage[currentSlot.color]++;
 			currentSlot.owner = playerIndex + 1;
 			_next_turn();
 		} else{
 			auctionIndex = players[playerIndex].slot;
 			timeLimit = block.timestamp + 2 minutes;
 		}
+	}
+
+	function upgrade(uint256 _slot, uint256 _buyTo, uint256 _amount) public started onlyTurn noAuction afterRoll { //buyTo starting from 1
+		Player storage currentPlayer = players[getPlayer[msg.sender] - 1];
+
+		require(slots[_slot].owner == getPlayer[msg.sender], "You are not the owner!");
+		require(_is_upgradeable(_slot, currentPlayer.slotsOwned) == true, "The progress of each slot you own is not enough!");
+
+		if (slots[currentPlayer.slot].progress < 5) {
+			if (_buyTo == 0)
+				require(houses > 0, "There are no houses left to buy!");
+			else
+				_buy_to(true, _buyTo, _amount);
+		} else {
+			if (_buyTo == 0)
+				require(hotels > 0, "There are no hotels left to buy!");
+			else
+				_buy_to(false, _buyTo, _amount);
+			return ;
+		}
+		require(currentPlayer.balance > 150, "You have insufficient funds!");
+		slots[_slot].progress++;
 	}
 
 	function bid(uint256 _amount) public started {
@@ -168,12 +197,11 @@ contract CryptoPoy {
 			}
 		} else {
 			players[auctionOwner - 1].balance -= auctionPrice;
-			if (_check_bankrupt(playerIndex) == true)
-				return ;
-			_own(playerIndex, players[playerIndex].slot);
+			_own(auctionOwner - 1, players[auctionOwner - 1].slot);
 			auctionIndex = 0;
 			auctionPrice = 0;
 			auctionOwner = 0;
+			_check_bankrupt(auctionOwner - 1);
 			_next_turn();
 		}
 	}
@@ -219,7 +247,7 @@ contract CryptoPoy {
 		}
 	}
 
-	function _next_turn() internal {
+	function _next_turn() internal { //check if broke players can still play (they shouldn't)
 		uint256 i = 1;
 
 		if (isReward == true) {
@@ -230,7 +258,7 @@ contract CryptoPoy {
 			doubleDiceStreak = 0;
 		while (players[turnIndex + i].balance == 0)
 			i++;
-		turnIndex = (turnIndex + i < playersAmount) ? turnIndex + i : turnIndex + i - playersAmount;
+		turnIndex = (turnIndex + i < players.length) ? turnIndex + i : turnIndex + i - players.length;
 		hasRolled = false;
 		timeLimit = block.timestamp + turnTime;
 	}
@@ -250,22 +278,50 @@ contract CryptoPoy {
 		}
 	}
 
-	function _free_properties(uint256 _playerIndex) internal returns(bool) {
+	function _check_bankrupt(uint256 _playerIndex) internal {
 		if (players[_playerIndex].balance == 0) {
+			Player storage currentPlayer = players[_playerIndex];
 
-			//pop player
-			return true;
+			playersAmount--;
+			for (uint256 i = 0; i < currentPlayer.slotsOwned.length; i++) {
+				slots[currentPlayer.slotsOwned[i]].owner = 0;
+			}
+			delete currentPlayer.slotsOwned;
 		}
-		return false;
 	}
 
+	function _is_upgradeable(uint256 _slot, uint256[] memory _slotsOwned) internal view returns (bool) {
+		uint256 color = slots[_slot].color;
+		uint256[4] memory sameColorArray;
+		uint256 array_length;
+		uint256 currentProgress = slots[_slot].progress;
+
+		for (uint256 i = 0; i < _slotsOwned.length; i++)
+			if (slots[_slotsOwned[i]].color == color) {
+				sameColorArray[array_length] = _slotsOwned[i];
+				array_length++;
+			}
+		require (array_length < colorsAmounts[color], "You don't have a monopoly!");
+		for (uint256 i = 0; i < array_length; i++)
+			if (currentProgress > slots[sameColorArray[i]].progress)
+				return false;
+		return true;
+	}
+
+	/* function _buy_to(bool _house, uint256 _playerIndex, uint256 _amount) internal {
+		if (_house == true)
+
+	} */
+
 	function _end_game() internal {
-		//repartir balance al ganador que siempre sera players[0] ya que es el que queda
+
+		for (uint256 i = 0; i < players.length; i++)
+			getPlayer[players[i].wallet] = 0;
 	}
 
 	function _own(uint256 _playerIndex, uint256 _slot) internal {
-		slots[_slot].owner = auctionOwner;
-		players[_playerIndex].heritage[slots[_slot].color]++;
+		slots[_slot].owner = _playerIndex + 1;
+		//players[_playerIndex].heritage[slots[_slot].color]++;
 		players[_playerIndex].slotsOwned.push(players[_playerIndex].slot);
 	}
 
